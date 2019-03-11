47a03c447da3baebe012b8033d134ad53e0c9197
Sakib Khan Inan <46783803+skinan@users.noreply.github.com>
2019-03-11 19:14:15 +0600

    Update Diamond_Shapes.py

--- Diamond_Shapes.py
+++ Diamond_Shapes.py
@@ -1,36 +1,36 @@
-n=int(input())
+n = int(input())
 
 for x in reversed(range(1, n+1)):
     for y in range(1, n+1):
-        if y>x:
+        if y > x:
             break
-        print("*", sep="", end="")
+        print("*", sep = "", end = "")
     i = int(x)
     i = ((n - i) * int(2)) + int(1)
-    while i!=0:
-        print(" ", sep="", end="")
-        i=i-1
+    while i != 0:
+        print(" ", sep ="", end = "")
+        i = i - 1
     for y in range(1 ,n+1):
-        if y>x:
+        if y > x:
             break
-        print("*", sep="", end="")
-    print("\n" ,end="")
+        print("*", sep = "", end = "")
+    print("\n", end = "")
 
-i=int(0)
+i = int(0)
 
-for x in range(2,n+1):
-    for y in range(1,n+1):
-        if y>x:
+for x in range(2, n+1):
+    for y in range(1, n+1):
+        if y > x:
             break
-        print("*", sep="", end="")
+        print("*", sep = "", end = "")
     i = int(x)
     i = ((n - i) * int(2)) + int(1)
-    while i!=0:
+    while i != 0:
         print(" ", sep="", end="")
-        i=i-1
-    for y in range(1,n+1):
-        if y>x:
+        i = i - 1
+    for y in range(1, n+1):
+        if y > x:
             break
-        print("*", sep="", end="")
-    print("\n", end="")
+        print("*", sep = "", end = "")
+    print("\n", end = "")
 
